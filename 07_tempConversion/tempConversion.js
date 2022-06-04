de8a8c1d1f3267e6ca117cea0c734292662a2e5c
Rene <renesuar99@gmail.com>
2022-06-03 20:27:23 -0400

    Finished 7th exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+let conversion = (temperature-32)*5/9; 
+Math.round(conversion);
+return conversion;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+let conversion = temperature*9/5+32; 
+Math.round(conversion);
+return conversion;
 };
 
 // Do not edit below this line
